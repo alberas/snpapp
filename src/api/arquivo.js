005d4afec1a90503490b730375d9b71ced0e3558
Alberi Lemos <alberi.depaula@gmail.com>
2020-02-13 15:54:59 -0300

    Correção do upload de imagens

--- src/api/arquivo.js
+++ src/api/arquivo.js
@@ -1,15 +1,20 @@
+import { Form } from "native-base";
+
 const API_URL = "http://snpmed.com.br/api";
 
 export const upload = async (idUsuario, bytArquivo) => {
     const url = API_URL + "/upload/" + idUsuario;
+    const frmData = new FormData();
+    frmData.append("byt_arquivo", bytArquivo);
+
     try {
         const response = await fetch(url,{
             method: "POST",
             headers: {
                 //'Content-Type': "multipart/form-data; charset=utf-8; boundary=" + Math.random().toString().substr(2)
-                "Content-Type" : "application/x-www-form-urlencoded"
+                //"Content-Type" : "application/x-www-form-urlencoded"
              },
-            body: "byt_arquivo=" + bytArquivo
+            body:  frmData
         });
         const responseJson = await response.json();
         return responseJson;
