087699de7168c7a591eba6bdaae2f4b0645b9dfa
Alberi Lemos <alberi.depaula@gmail.com>
2020-04-20 22:10:00 -0300

    INCLUSÃO DE IMAGENS

---
+++ web-build/precache-manifest.5f25adedb4ca1053aa09aa33bcd555ba.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc18d2808008b3559bd",
+    "url": "/static/js/app.73f1d755.chunk.js"
+  },
+  {
+    "revision": "2761cd8426f52ed76a3e",
+    "url": "/static/js/runtime~app.43360584.js"
+  },
+  {
+    "revision": "c8c023d84f8558484a87",
+    "url": "/static/js/2.446ee0d8.chunk.js"
+  },
+  {
+    "revision": "9ea894f2b83776493ae434a5320d6cba",
+    "url": "/static/media/default-avatar.9ea894f2.jpg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "fdebdc3464d2bc2d9fff2a52dc4eb81a",
+    "url": "/static/media/FFFF00.fdebdc34.png"
+  },
+  {
+    "revision": "6501a54cd12a718f622a97e01839331c",
+    "url": "/static/media/FFA500.6501a54c.png"
+  },
+  {
+    "revision": "8e84e39c9264f06d8e8f91c438842c77",
+    "url": "/static/media/FF1493.8e84e39c.png"
+  },
+  {
+    "revision": "e50e9ca9fb9ac45a8cde80bb3ce195a3",
+    "url": "/static/media/BCD2EE.e50e9ca9.png"
+  },
+  {
+    "revision": "49088b0a1e26d88e2149078daf48ab74",
+    "url": "/static/media/7FFF00.49088b0a.png"
+  },
+  {
+    "revision": "afe33bdafae0aef504cb919be374c653",
+    "url": "/static/media/1E90FF.afe33bda.png"
+  },
+  {
+    "revision": "3c48fc6a7c39d52bfcc82242b45c7bc8",
+    "url": "/static/media/ico0.3c48fc6a.png"
+  },
+  {
+    "revision": "b78a91f23a9b9e61b92d174807ca2270",
+    "url": "/static/media/bg.b78a91f2.jpg"
+  },
+  {
+    "revision": "03db19f39b170fd7b9dbb7cf0b9b595c",
+    "url": "/static/media/ico2.03db19f3.png"
+  },
+  {
+    "revision": "47392c681615d9c4e9b6549e897efec1",
+    "url": "/static/media/ico1.47392c68.png"
+  },
+  {
+    "revision": "c35ff7d796f4690df7bdcf103c95a66d",
+    "url": "/static/media/bt1.c35ff7d7.png"
+  },
+  {
+    "revision": "5f60526252f5fa017aac4d289671af45",
+    "url": "/static/media/bt2.5f605262.png"
+  },
+  {
+    "revision": "f3db710a7f948dc78c0c3ea307be46a3",
+    "url": "/static/media/bt3.f3db710a.png"
+  },
+  {
+    "revision": "fa5c724ec1e74e57557e230054c20f6d",
+    "url": "/static/media/bt5.fa5c724e.png"
+  },
+  {
+    "revision": "11a6fce74a74ae177fbd20f599fcb346",
+    "url": "/static/media/i_whatsapp.11a6fce7.png"
+  },
+  {
+    "revision": "1edd5017f455b09c608e175b1285c06d",
+    "url": "/static/media/date_icon.1edd5017.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e9841d5ffc5494a08d651c96c330f13",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5cd221e1f00589d415133b1afdf680e8",
+    "url": "/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "98bb6a9f670dc5a2d51c428f77539a44",
+    "url": "/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "bd8f2ec0dc97f31208df301369367bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "faaec5cc5f1f0152f525ddcd268456a5",
+    "url": "/static/js/runtime~app.43360584.js.gz"
+  },
+  {
+    "revision": "e23f53fc7d81ff2c2521f0239c162053",
+    "url": "/static/js/app.73f1d755.chunk.js.gz"
+  },
+  {
+    "revision": "77118b7f4c9495d3ff8494f06b91544c",
+    "url": "/static/js/2.446ee0d8.chunk.js.gz"
+  }
+];
